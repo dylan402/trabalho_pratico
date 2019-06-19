abfe17bd38277663a213336ead46b26b29854b8c
Dylan Santiago <dylansantiagomelo@gmail.com>
2019-06-19 19:13:01 -0300

    Adição e estilização da pagina index Adição de rotas

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("Servidor iniciado.");
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + '/view'));
+
+app.use(require('./routes'));
+
+app.listen(8081, () => {
+    console.log("Servidor iniciado na porta 8081.");
+});
